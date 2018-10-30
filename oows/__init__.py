1df3da4a88af790695cb8bd310c770d9c782264e
Felipe Rodrigues <felipe@felipevr.com>
2018-10-30 09:53:52 -0300

    Add author info

--- oows/__init__.py
+++ oows/__init__.py
@@ -1,1 +1,10 @@
+"""
+OOWS
+
+An object-oriented friendly & pythonic client for Amazon Web Services (AWS)
+"""
 from . import ecs
+
+__author__ = "Felipe V. Rodrigues"
+__license__ = "MIT"
+__version__ = "0.0.4"
